9abd05eaeb054a42c94f50f530e2a685576e0b2f
Cyan <cyan4.game@gmail.com>
2021-05-03 15:59:16 +0400

    update

--- component/comLazyImg_vue.js
+++ component/comLazyImg_vue.js
@@ -4,11 +4,21 @@ function _loadComLazyImg () {
     Vue.component('lazy-img', {
         name: 'lazy-img',
         props: ['src'],
+        data: () => {
+            return {
+                loading: true
+            }
+        },
+        methods: {
+            loaded (e) {
+                this.loading = false
+            }
+        },
         template: `
         <div class="com-lazy-img">
-            <div class="com-lazy-bg"></div>
-            <div class="com-lazy-flash"></div>
-            <img :src="src" alt="logo">
+            <div class="com-lazy-bg" v-if="loading"></div>
+            <div class="com-lazy-flash" v-if="loading"></div>
+            <img :src="src" alt="logo" @load="loaded">
         </div>
         `
     })
