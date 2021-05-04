b2bf53a47f73a72c8b41a45e52a5f6272eec77bc
Cyan <cyan4.game@gmail.com>
2021-05-04 17:01:19 +0400

    update

--- common/js/public.js
+++ common/js/public.js
@@ -3,7 +3,8 @@
 
     const jsArr = [
         '../../common/js/vue2.6.12.min.js',
-        '../../common/js/rem.js'
+        '../../common/js/rem.js',
+        '../../common/js/crypto-js.js'
     ]
 
     jsArr.forEach(e => {
@@ -29,3 +30,23 @@ function _cancelLoadingFrame () {
     }
 }
 
+
+// 图片解密
+function _decrypt (word) {
+    const str = decodeURIComponent(word)
+    // console.log(str)
+    // const str = word
+    const aesKey = '2c4add8f849a7bea'
+    const aesVi = 'dc4b73b33e69eaff'
+    const key = aesKey
+    const iv = aesVi
+    // const key = CryptoJS.enc.Utf8.parse(aesKey)
+    // const iv = CryptoJS.enc.Utf8.parse(aesVi)
+    const decrypt = CryptoJS.AES.decrypt(str, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    })
+    // console.log(decrypt.toString())
+    // return decrypt.toString(CryptoJS.enc.Utf8)
+}
